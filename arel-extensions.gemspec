c4db9c2fd9bd67b3e88a740e3fa1d34184497164
Jon Bracy <jonbracy@gmail.com>
2017-03-31 12:30:08 -0700

    Update contains and contained_by predications to support JSON as well

--- arel-extensions.gemspec
+++ arel-extensions.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |gem|
   gem.name = 'arel-extensions'
-  gem.version = '1.1.0'
+  gem.version = '1.2.0'
   
   gem.authors       = ["Jon Bracy"]
   gem.email         = ["jonbracy@gmail.com"]
--- lib/arel/array_predications.rb
+++ lib/arel/array_predications.rb
@@ -1,14 +1,14 @@
 module Arel
   module ArrayPredications
     
-    def contains(*values)
-      values = values[0] if values.size == 1 && values[0].is_a?(Array)
-      Arel::Nodes::Contains.new(self, values)
+    # Used by both JSON and ARRAY so it doesn't try to cast to array
+    def contains(value)
+      Arel::Nodes::Contains.new(self, value)
     end
     
-    def contained_by(*values)
-      values = values[0] if values.size == 1 && values[0].is_a?(Array)
-      Arel::Nodes::ContainedBy.new(self, values)
+    # Used by both JSON and ARRAY so it doesn't try to cast to array
+    def contained_by(value)
+      Arel::Nodes::ContainedBy.new(self, value)
     end
     
     def overlaps(*values)
