8bad894021bd174fddd013a9fe91c70c47f0d865
Jon Bracy <jonbracy@gmail.com>
2019-07-22 15:11:41 -0500

    Perdications now quote nodes

--- lib/arel/array_predications.rb
+++ lib/arel/array_predications.rb
@@ -3,21 +3,21 @@ module Arel
     
     # Used by both JSON and ARRAY so it doesn't try to cast to array
     def contains(value)
-      Arel::Nodes::Contains.new(self, value)
+      Arel::Nodes::Contains.new(self, quoted_node(value))
     end
     
     # Used by both JSON and ARRAY so it doesn't try to cast to array
     def contained_by(value)
-      Arel::Nodes::ContainedBy.new(self, value)
+      Arel::Nodes::ContainedBy.new(self, quoted_node(value))
     end
     
     def excludes(value)
-      Arel::Nodes::Excludes.new(self, value)
+      Arel::Nodes::Excludes.new(self, quoted_node(value))
     end
     
     def overlaps(*values)
       values = values[0] if values.size == 1 && values[0].is_a?(Array)
-      Arel::Nodes::Overlaps.new(self, values)
+      Arel::Nodes::Overlaps.new(self, values.map { |v| quoted_node(v) })
     end
     
   end
