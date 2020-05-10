4465fc80923fe74f7fa1173f73ab083c9577ec77
Jon Bracy <jonbracy@gmail.com>
2020-05-10 16:13:41 -0400

    Update TSPredication to use keyword args

--- lib/arel/ts_predications.rb
+++ lib/arel/ts_predications.rb
@@ -2,8 +2,8 @@ module Arel
   module TSPredications
     
     def ts_query(expression, language=nil)
-      vector = Arel::Nodes::TSVector.new(self, language)
-      query = Arel::Nodes::TSQuery.new(expression, language)
+      vector = Arel::Nodes::TSVector.new(self, language: language)
+      query = Arel::Nodes::TSQuery.new(expression, language: language)
       
       Arel::Nodes::TSMatch.new(vector, query)
     end
